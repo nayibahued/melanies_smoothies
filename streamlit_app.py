0db3ba11c6515b495aa3699f7feb42e408f94e6a
nayibahued <67173808+nayibahued@users.noreply.github.com>
2025-08-17 14:12:46 -0600

    Update streamlit_app.py
    i had the pending orders app intead of placing the order

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ from snowflake.snowpark.functions import col, when_matched
 
 
 # Write directly to the app
-st.title(f":cup_with_straw: Pending smoothies orders :cup_with_straw:")
+st.title(f":cup_with_straw: Customize your Smoothie! :cup_with_straw:")
 st.write(
-  """Orders that need to be filled
+  """Choose the fruits you want in your smoothie
   """
 )
 
@@ -14,28 +14,33 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 
-my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+
+ingredients_list = st.multiselect('Choose up to five ingredients:',
+                                  my_dataframe,max_selections=5)
+
+name_on_order = st.text_input('Name on smoothie')
+st.write('The name on smoothie will be: ',name_on_order)
 
 
-if my_dataframe:    
-    editable_df = st.data_editor(my_dataframe)
-    
-    submitted = st.button('Submit')
-    
-    if submitted:
-        st.success('Someone clicked the button', icon = '👍')
+if ingredients_list:
+    #st.write(ingredients_list)
+    #st.text(ingredients_list)
+
+    ingredients_string=''
+    for fruit_chosen in ingredients_list:
+        ingredients_string+=fruit_chosen+' '
+    st.write(ingredients_string)
+
+     
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+    st.write(my_insert_stmt)
+
+    time_to_insert = st.button('Submit')
+
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
         
-        try:
-            og_dataset = session.table("smoothies.public.orders")
-            edited_dataset = session.create_dataframe(editable_df)
-            og_dataset.merge(edited_dataset
-                                 , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
-                                 , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
-                                )
-            st.success('Order(s) updated', icon = '👍')
-        except:
-            st.write('Something went wrong')
-
-else:
-    st.success('There are no pending orders', icon = '👍')
-    
+        st.success('Your Smoothie is ordered!', icon="✅")
